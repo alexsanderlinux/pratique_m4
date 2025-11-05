64dccc1f47490a0ac23d325065872a1bda2d44af
alexsanderlinux <alexsanderlinux@gmail.com>
2025-11-05 02:02:23 -0300

    praticando javaScript

---
+++ calculadora.js
@@ -0,0 +1,23 @@
+function calcularDesconto(){
+
+    //Entrada
+    let valorOriginal = document.getElementById("valor").value;
+    let desconto = document.getElementById("desconto").value;
+
+    //processamento
+    let valorDesconto = (valorOriginal * desconto) / 100;
+    let valorFinal = valorOriginal - valorDesconto;
+
+
+
+    //saida
+
+    document.getElementById("resultado").textContent = "Valor final: " + valorFinal;
+
+
+}
+
+
+
+
+
